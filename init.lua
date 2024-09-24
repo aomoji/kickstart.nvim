cf6514d986e1b627a614c1fb012aee2c46c947c8
Kaito Oshiro <k.oshiro@churadata.okinawa>
2024-09-24 16:17:29 +0900

    [add] dbt's snippets

--- init.lua
+++ init.lua
@@ -768,6 +768,7 @@ require('lazy').setup({
             'rafamadriz/friendly-snippets',
             config = function()
               require('luasnip.loaders.from_vscode').lazy_load()
+              require('luasnip.loaders.from_vscode').lazy_load { paths = './my_snippets' }
             end,
           },
         },
