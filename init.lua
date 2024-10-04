175bafd6b415e9329a6b452bd3c62cf9b445eb0c
Kaito Oshiro <k.oshiro@churadata.okinawa>
2024-10-04 18:53:04 +0900

    [add] some cmp

--- init.lua
+++ init.lua
@@ -732,6 +732,10 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
+      'lukas-reineke/cmp-rg',
+      'ray-x/cmp-sql',
+      'chrisgrieser/cmp_yanky',
     },
     config = function()
       -- See `:help cmp`
@@ -808,115 +812,119 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'sql' },
+          { name = 'buffer' },
+          { name = 'rg' },
+          { name = 'cmp_yanky' },
         },
       }
     end,
   },
 
-  {
-    'rose-pine/neovim',
-    name = 'rose-pine',
-    config = function()
-      require('rose-pine').setup {
-        variant = 'auto', -- auto, main, moon, or dawn
-        dark_variant = 'main', -- main, moon, or dawn
-        dim_inactive_windows = false,
-        extend_background_behind_borders = true,
-
-        enable = {
-          terminal = true,
-          legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
-          migrations = true, -- Handle deprecated options automatically
-        },
-
-        styles = {
-          bold = true,
-          italic = true,
-          transparency = false,
-        },
-
-        groups = {
-          border = 'muted',
-          link = 'iris',
-          panel = 'surface',
-
-          error = 'love',
-          hint = 'iris',
-          info = 'foam',
-          note = 'pine',
-          todo = 'rose',
-          warn = 'gold',
-
-          git_add = 'foam',
-          git_change = 'rose',
-          git_delete = 'love',
-          git_dirty = 'rose',
-          git_ignore = 'muted',
-          git_merge = 'iris',
-          git_rename = 'pine',
-          git_stage = 'iris',
-          git_text = 'rose',
-          git_untracked = 'subtle',
-
-          h1 = 'iris',
-          h2 = 'foam',
-          h3 = 'rose',
-          h4 = 'gold',
-          h5 = 'pine',
-          h6 = 'foam',
-        },
-
-        palette = {
-          -- Override the builtin palette per variant
-          -- moon = {
-          --     base = '#18191a',
-          --     overlay = '#363738',
-          -- },
-        },
-
-        highlight_groups = {
-          -- Comment = { fg = "foam" },
-          -- VertSplit = { fg = "muted", bg = "muted" },
-        },
-
-        before_highlight = function(group, highlight, palette)
-          -- Disable all undercurls
-          -- if highlight.undercurl then
-          --     highlight.undercurl = false
-          -- end
-          --
-          -- Change palette colour
-          -- if highlight.fg == palette.pine then
-          --     highlight.fg = palette.foam
-          -- end
-        end,
-      }
-
-      vim.cmd 'colorscheme rose-pine'
-      -- vim.cmd 'colorscheme rose-pine-main'
-      -- vim.cmd 'colorscheme rose-pine-moon'
-      -- vim.cmd 'colorscheme rose-pine-dawn'
-    end,
-  },
-
-  -- { -- You can easily change to a different colorscheme.
-  --   -- Change the name of the colorscheme plugin below, and then
-  --   -- change the command in the config to whatever the name of that colorscheme is.
-  --   --
-  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-  --   'dam9000/colorscheme-midnightblue',
-  --   priority = 1000, -- Make sure to load this before all the other start plugins.
-  --   init = function()
-  --     -- Load the colorscheme here.
-  --     -- Like many other themes, this one has different styles, and you could load
-  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-  --     vim.cmd.colorscheme 'midnightblue'
+  -- {
+  --   'rose-pine/neovim',
+  --   name = 'rose-pine',
+  --   config = function()
+  --     require('rose-pine').setup {
+  --       variant = 'auto', -- auto, main, moon, or dawn
+  --       dark_variant = 'main', -- main, moon, or dawn
+  --       dim_inactive_windows = false,
+  --       extend_background_behind_borders = true,
   --
-  --     -- You can configure highlights by doing something like:
-  --     vim.o.background = 'dark'
-  --     vim.cmd.hi 'Comment gui=none'
+  --       enable = {
+  --         terminal = true,
+  --         legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+  --         migrations = true, -- Handle deprecated options automatically
+  --       },
+  --
+  --       styles = {
+  --         bold = true,
+  --         italic = true,
+  --         transparency = false,
+  --       },
+  --
+  --       groups = {
+  --         border = 'muted',
+  --         link = 'iris',
+  --         panel = 'surface',
+  --
+  --         error = 'love',
+  --         hint = 'iris',
+  --         info = 'foam',
+  --         note = 'pine',
+  --         todo = 'rose',
+  --         warn = 'gold',
+  --
+  --         git_add = 'foam',
+  --         git_change = 'rose',
+  --         git_delete = 'love',
+  --         git_dirty = 'rose',
+  --         git_ignore = 'muted',
+  --         git_merge = 'iris',
+  --         git_rename = 'pine',
+  --         git_stage = 'iris',
+  --         git_text = 'rose',
+  --         git_untracked = 'subtle',
+  --
+  --         h1 = 'iris',
+  --         h2 = 'foam',
+  --         h3 = 'rose',
+  --         h4 = 'gold',
+  --         h5 = 'pine',
+  --         h6 = 'foam',
+  --       },
+  --
+  --       palette = {
+  --         -- Override the builtin palette per variant
+  --         -- moon = {
+  --         --     base = '#18191a',
+  --         --     overlay = '#363738',
+  --         -- },
+  --       },
+  --
+  --       highlight_groups = {
+  --         -- Comment = { fg = "foam" },
+  --         -- VertSplit = { fg = "muted", bg = "muted" },
+  --       },
+  --
+  --       before_highlight = function(group, highlight, palette)
+  --         -- Disable all undercurls
+  --         -- if highlight.undercurl then
+  --         --     highlight.undercurl = false
+  --         -- end
+  --         --
+  --         -- Change palette colour
+  --         -- if highlight.fg == palette.pine then
+  --         --     highlight.fg = palette.foam
+  --         -- end
+  --       end,
+  --     }
+  --
+  --     vim.cmd 'colorscheme rose-pine'
+  --     -- vim.cmd 'colorscheme rose-pine-main'
+  --     -- vim.cmd 'colorscheme rose-pine-moon'
+  --     -- vim.cmd 'colorscheme rose-pine-dawn'
   --   end,
   -- },
+
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'dam9000/colorscheme-midnightblue',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'midnightblue'
+
+      -- You can configure highlights by doing something like:
+      vim.o.background = 'dark'
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
 
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
