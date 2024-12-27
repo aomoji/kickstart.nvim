e25cf4e7a63edb260cbffc69b640042de61493d8
Kaito Oshiro <k.oshiro@churadata.okinawa>
2024-12-27 18:29:47 +0900

    changed colorschema

--- init.lua
+++ init.lua
@@ -986,6 +986,7 @@ require('lazy').setup({
       vim.cmd.colorscheme 'everforest'
       vim.g.everforest_background = 'hard'
       vim.g.everforest_better_performance = 1
+      vim.g.everforest_ui_contrast = 'high'
     end,
   },
 
